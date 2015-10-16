2abf66eb66cdc5b77a2dfca3f0a8219c40172c66
Karthikeya Udupa K M <karthikeyaudupa@gmail.com>
2015-10-16 21:53:01 +0200

    Migrated to Swift 2.0

--- Demo/LineNumber/Document.swift
+++ Demo/LineNumber/Document.swift
@@ -43,24 +43,22 @@ class Document: NSDocument {
 
 	override func makeWindowControllers() {
 		// Returns the Storyboard that contains your Document window.
-		let storyboard = NSStoryboard(name: "Main", bundle: nil)!
+		let storyboard = NSStoryboard(name: "Main", bundle: nil)
 		let windowController = storyboard.instantiateControllerWithIdentifier("Document Window Controller") as! NSWindowController
 		self.addWindowController(windowController)
 	}
 
-	override func dataOfType(typeName: String, error outError: NSErrorPointer) -> NSData? {
+	override func dataOfType(typeName: String) throws -> NSData {
 		// Insert code here to write your document to data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning nil.
 		// You can also choose to override fileWrapperOfType:error:, writeToURL:ofType:error:, or writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-		outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
-		return nil
+		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
 	}
 
-	override func readFromData(data: NSData, ofType typeName: String, error outError: NSErrorPointer) -> Bool {
+	override func readFromData(data: NSData, ofType typeName: String) throws {
 		// Insert code here to read your document from the given data of the specified type. If outError != nil, ensure that you create and set an appropriate error when returning false.
 		// You can also choose to override readFromFileWrapper:ofType:error: or readFromURL:ofType:error: instead.
 		// If you override either of these, you should also override -isEntireFileLoaded to return NO if the contents are lazily loaded.
-		outError.memory = NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
-		return false
+		throw NSError(domain: NSOSStatusErrorDomain, code: unimpErr, userInfo: nil)
 	}
 
 
